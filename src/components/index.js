2ae098c58aef4c7971b419996de426415589f0bf
Chavdar Dimov <chavdar.dimovv@gmail.com>
2024-05-14 17:37:39 +0300

    push whole project

---
+++ src/components/index.js
@@ -0,0 +1,33 @@
+import Box from "./Box";
+import ErrorMessage from "./ErrorMessage"; //
+import Loader from "./Loader";
+import Logo from "./Logo";
+import Main from "./Main";
+import Movie from "./Movie";
+import MovieDetails from "./MovieDetails";
+import MovieList from "./MovieList";
+import NavBar from "./NavBar";
+import NumResults from "./NumResults";
+import Search from "./Search";
+import StarRating from "./StarRating";
+import WatchedMovie from "./WatchedMovie";
+import WatchedMoviesList from "./WatchedMoviesList";
+import WatchedSummary from "./WatchedSummary";
+
+export {
+  Box,
+  ErrorMessage,
+  Loader,
+  Logo,
+  Main,
+  Movie,
+  MovieDetails,
+  MovieList,
+  NavBar,
+  NumResults,
+  Search,
+  StarRating,
+  WatchedMovie,
+  WatchedMoviesList,
+  WatchedSummary,
+};
